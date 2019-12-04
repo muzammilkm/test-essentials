92fe72cb292677dabc3d555b65858f58282e41c9
Muzammil Khaja Mohammed <muzammilkm@gmail.com>
2019-12-04 16:47:22 +0530

    adding mockserver to toolkit

---
+++ src/TestEssentials.ToolKit.MockServer/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using Topshelf;
+
+namespace TestEssentials.ToolKit.MockServer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Any(x => x.Contains("--help")))
+            {
+                DisplayHelp();
+                Environment.ExitCode = 0;
+                return;
+            }
+
+            var map = "";
+            var useSsl = false;
+            var port = 36987;
+            var assemblyFile = "";
+            var typeName = "";
+
+
+            var topshelfExitCode = HostFactory.Run(x =>
+            {
+                x.AddCommandLineDefinition("map", v => map = v);
+                x.AddCommandLineDefinition("port", v => port = Convert.ToInt32(v));
+                x.AddCommandLineSwitch("ssl", v => useSsl = v);
+                x.AddCommandLineDefinition("assembly", v => assemblyFile = v);
+                x.AddCommandLineDefinition("types", v => typeName = v);
+
+                x.Service<WireMockService>(sc =>
+                {
+                    sc.ConstructUsing(() => new WireMockService(map, port, useSsl, assemblyFile, typeName));
+                    sc.WhenStarted((s, hc) => s.Start(hc));
+                    sc.WhenStopped((s, hc) => s.Stop(hc));
+                });
+                x.EnableServiceRecovery(rc =>
+                {
+                    rc.RestartService(5);
+                });
+                x.RunAsLocalSystem();
+                x.SetDescription("Mock Server");
+                x.SetDisplayName("Mock Server");
+                x.SetServiceName("MockServer");
+            });
+
+            var exitCode = (int) Convert.ChangeType(topshelfExitCode, topshelfExitCode.GetTypeCode());
+            Environment.ExitCode = exitCode;
+        }
+
+        private static void DisplayHelp()
+        {
+            Console.WriteLine("Mock Server");
+            Console.WriteLine("");
+            Console.WriteLine("Useage: ");
+            Console.WriteLine("");
+            Console.WriteLine("Options:");
+            Console.WriteLine("\t--help\t\tShow help");
+            Console.WriteLine("\t--map\t\tLocation to mapping files.");
+            Console.WriteLine("\t--ssl\t\tEnable SSL. [default: false]");
+            Console.WriteLine("\t--port\t\tPort to listen on [default:36987].");
+            Console.WriteLine("\t--assembly\t\tLoad custom handlebar assembly [default:''].");
+            Console.WriteLine("\t--type\t\tComplete Namespace of custom handlebar in assembly [default:''].");
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ src/TestEssentials.ToolKit.MockServer/WireMockService.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TestEssentials.ToolKit.WireMock;
+using Topshelf;
+using WireMock.Handlers;
+using WireMock.Logging;
+using WireMock.Net.StandAlone;
+using WireMock.Server;
+using WireMock.Settings;
+
+namespace TestEssentials.ToolKit.MockServer
+{
+    public class WireMockService : ServiceControl
+    {
+        private readonly WireMockConsoleLogger _logger;
+        private readonly IFileSystemHandler _fileHandler;
+        private readonly bool _useSsl;
+        private readonly int? _port;
+
+        private FluentMockServer _server;
+        private readonly string _assemblyFile;
+        private readonly string _typeName;
+
+        #region Ctor
+        public WireMockService(string map, int port, bool useSsl, string assemblyFile, string typeName)
+        {
+            _port = port;
+            _useSsl = useSsl;
+            _fileHandler = new LocalFileSystemHandler(map);
+            _assemblyFile = assemblyFile;
+            _typeName = typeName;
+            _logger = new WireMockConsoleLogger();
+        }
+        #endregion
+
+        public bool Start(HostControl hostControl)
+        {
+
+            _logger.Debug("Starting...");
+
+            var handleBarTransformers = new List<IHandleBarTransformer>();
+
+            try
+            {   
+                var currentAssembly = Assembly.LoadFrom(_assemblyFile);
+                var item = currentAssembly.GetType(_typeName);
+                handleBarTransformers.Add((IHandleBarTransformer) Activator.CreateInstance(item));
+            }
+            catch (Exception ex)
+            {
+                // ignored
+            }
+
+
+            var settings = new FluentMockServerSettings()
+            {
+                UseSSL = _useSsl,
+                Port = _port,
+                ReadStaticMappings = true,
+                WatchStaticMappings = true,
+                WatchStaticMappingsInSubdirectories = true,
+                AllowPartialMapping = true,
+                StartAdminInterface = true,
+                Logger = _logger,
+                FileSystemHandler = _fileHandler,
+                HandlebarsRegistrationCallback = (a, b) =>
+                {
+                    foreach (var handleBarTransformer in handleBarTransformers)
+                    {
+                        a.RegisterHelper(handleBarTransformer.Name, handleBarTransformer.Render);
+                    }
+                }
+            };
+            _server = StandAloneApp.Start(settings);
+            _logger.Debug("Started");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            _logger.Debug("Stopping...");
+            _server.Stop();
+            _logger.Debug("Stopped");
+            return true;
+        }
+
+    }
+}
